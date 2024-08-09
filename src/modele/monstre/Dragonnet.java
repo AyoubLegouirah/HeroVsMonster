5b5b53d53160f49b68bde72cbc7581d2a1c78b7a
AyoubLegouirah <ayoubgrand@outlook.fr>
2024-08-09 15:09:33 +0200

    git init -m

--- src/modele/monstre/Dragonnet.java
+++ src/modele/monstre/Dragonnet.java
@@ -7,11 +7,13 @@ public class Dragonnet extends Monstre  {
         super(1,0);
 
         int nbrCuir = Des.D4.roll(1,1);
-
+        /*
         if(Monstre.pv <= 0){
             nbrCuir--;
             System.out.println("Dragonnet a donner " + nbrCuir + " cuir()");
         }
+
+         */
 
 
     }
